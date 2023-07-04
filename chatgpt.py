8c0e4e4a244204da09b5cd3a0768b8141b92b9ef
jesse berkowitz <jberkow@brandeis.edu>
2023-07-04 19:50:24 -0400

    chatgpt

---
+++ chatgpt.py
@@ -0,0 +1,17 @@
+import openai
+api_key = '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'
+
+def answer(max_tokens):
+    question = input('What is your question?\n') 
+    answer = openai.ChatCompletion.create(            
+            api_key=api_key,
+            model="gpt-3.5-turbo",
+            max_tokens=max_tokens,
+            messages=[        
+                    {"role": "user", "content": question}
+                ]
+            )['choices'][0]['message']['content']
+    sentences = answer.split('.')
+    print('.'.join(sentences[:-1])+'.')
+
+answer(50)
